a202f2cbec8fc290776ad557f5fdc7d8df4153de
A01749675 <A01749675@tec.mx>
2023-10-12 20:26:00 -0600

    Minor correction

--- qr_navigation_2/follow_gps.py
+++ qr_navigation_2/follow_gps.py
@@ -29,25 +29,6 @@ class FollowGPS(Node):
 		self.linear_velocity = 0.33
 		self.angular_velocity = 0.2
 
-	def euler_from_quaternion(self,x, y, z, w):
-	        """
-	        Convert a quaternion into euler angles (roll, pitch, yaw)
-	        roll is rotation around x in radians (counterclockwise)
-	        pitch is rotation around y in radians (counterclockwise)
-	        yaw is rotation around z in radians (counterclockwise)
-	        """
-	        t0 = +2.0 * (w * x + y * z)
-	        t1 = +1.0 - 2.0 * (x * x + y * y)
-	        roll_x = math.atan2(t0, t1)
-	        t2 = +2.0 * (w * y - z * x)
-	        t2 = +1.0 if t2 > +1.0 else t2
-	        t2 = -1.0 if t2 < -1.0 else t2
-	        pitch_y = math.asin(t2)
-	        t3 = +2.0 * (w * z + x * y)
-	        t4 = +1.0 - 2.0 * (y * y + z * z)
-	        yaw_z = math.atan2(t3, t4)
-	     
-	        return roll_x, pitch_y, yaw_z # in radians
 
 	def FollowGPS_callback(self,request,response):
 		
@@ -77,7 +58,22 @@ class FollowGPS(Node):
 		self.cmd_vel.publish(self.twist)
 		self.orglong = self.gps_coordinates[1]
 		self.orglat = self.gps_coordinates[0]
+		response.arrived = True 
+		return response
 
+	def euler_from_quaternion(self,x, y, z, w):
+		t0 = +2.0 * (w * x + y * z)
+		t1 = +1.0 - 2.0 * (x * x + y * y)
+		roll_x = math.atan2(t0, t1)
+		t2 = +2.0 * (w * y - z * x)
+		t2 = +1.0 if t2 > +1.0 else t2
+		t2 = -1.0 if t2 < -1.0 else t2
+		pitch_y = math.asin(t2)
+		t3 = +2.0 * (w * z + x * y)
+		t4 = +1.0 - 2.0 * (y * y + z * z)
+		yaw_z = math.atan2(t3, t4)
+		return roll_x, pitch_y, yaw_z # in radians
+	
 	def update_angle(self,msg):
 		quat = Quaternion()
 		quat = msg.orientation
