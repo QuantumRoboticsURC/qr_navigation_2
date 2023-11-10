7b762aaabcd7411946848fa003fbe3f4abd774e4
MarzyHJ <133036370+MarzyHJ@users.noreply.github.com>
2023-11-10 12:41:45 -0600

    Update node_controller.py

--- qr_navigation_2/node_controller.py
+++ qr_navigation_2/node_controller.py
@@ -1,32 +1,80 @@
 import rclpy
 from rclpy.node import Node
-from custom_interfaces.srv import FollowGPS
+from std_msgs.msg import Bool, Int32
+from geometry_msgs.msg import Twist
 
 class NodeController(Node):
     def __init__(self):
         super().__init__('node_controller')
-        self.client = self.create_client(FollowGPS, 'follow_gps')
-        while not self.client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-        self.req = FollowGPS.Request()
 
-    def send_gps_coordinates(self, latitude, longitude):
-        self.req.latitude = latitude
-        self.req.longitude = longitude
-        self.future = self.client.call_async(self.req)
-        rclpy.spin_until_future_complete(self, self.future)
-        if self.future.result() is not None:
-            self.get_logger().info('Response: %r' % (self.future.result().arrived))
+        # Suscripciones
+        self.create_subscription(Twist, 'cmd_vel_ca', self.cmd_vel_ca_callback, 10)
+        self.create_subscription(Bool, 'arrived_ca', self.arrived_ca_callback, 10)
+        self.create_subscription(Int32, 'target_type', self.target_type_callback, 10)
+        self.create_subscription(Twist, 'cmd_vel_fg', self.cmd_vel_fg_callback, 10)
+        self.create_subscription(Bool, 'arrived_fg', self.arrived_fg_callback, 10)
+        self.create_subscription(Bool, 'arrived_sr', self.arrived_sr_callback, 10)
+        self.create_subscription(Twist, 'cmd_vel_sr', self.cmd_vel_sr_callback, 10)
+
+        # Publicadores
+        self.pub_arrived = self.create_publisher(Bool, 'arrived', 10)
+        self.pub_cmd_vel = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.pub_state = self.create_publisher(Int32, 'state', 10)
+
+        # Variables de estado porque si no me pierdo :c
+        self.arrived = False
+        self.cmd_vel = Twist()
+        self.state = Int32()
+
+    # Callbacks para los tópicos suscritos
+    def cmd_vel_ca_callback(self, msg):
+        if self.arrived:
+            self.cmd_vel = msg
+            self.pub_cmd_vel.publish(self.cmd_vel)
+
+    def arrived_ca_callback(self, msg):
+        self.check_arrived(msg)
+
+    def target_type_callback(self, msg):
+        if msg.data == 0:
+            self.state.data = 0
+            self.pub_state.publish(self.state)
+        elif msg.data == 1 and not self.arrived:
+            self.state.data = 4
+            self.pub_state.publish(self.state)
+        elif msg.data in [2, 3] and not self.arrived:
+            self.state.data = 3 if msg.data == 2 else 2
+            self.pub_state.publish(self.state)
+
+    def cmd_vel_fg_callback(self, msg):
+        if self.state.data == 0:
+            self.cmd_vel = msg
+            self.pub_cmd_vel.publish(self.cmd_vel)
+
+    def arrived_fg_callback(self, msg):
+        self.check_arrived(msg)
+
+    def arrived_sr_callback(self, msg):
+        self.check_arrived(msg)
+
+    def cmd_vel_sr_callback(self, msg):
+        if self.state.data in [1, 2, 3] and not self.arrived:
+            self.cmd_vel = msg
+            self.pub_cmd_vel.publish(self.cmd_vel)
+
+    def check_arrived(self, msg):
+        if msg.data:
+            self.arrived = True
+            arrived_msg = Bool()
+            arrived_msg.data = True
+            self.pub_arrived.publish(arrived_msg)
         else:
-            self.get_logger().error('Exception while calling service: %r' % (self.future.exception()))
+            self.arrived = False
 
 def main(args=None):
     rclpy.init(args=args)
     node_controller = NodeController()
-    # Definir las coordenadas 
-    latitude = -34.603722
-    longitude = -58.381592
-    node_controller.send_gps_coordinates(latitude, longitude)
+    rclpy.spin(node_controller)
     node_controller.destroy_node()
     rclpy.shutdown()
 
