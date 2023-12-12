c464f31a7f7204637ad67887fce8627dfb1d0857
MarzyHJ <133036370+MarzyHJ@users.noreply.github.com>
2023-12-12 14:47:54 -0600

    Update target.py

--- qr_navigation_2/target.py
+++ qr_navigation_2/target.py
@@ -1,21 +1,36 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool, Int8
+from std_msgs.msg import Int8
 
 class Target(Node):
-    def __init__ (self):
+    def __init__(self):
         super().__init__('node_controller')
 
         # Publicador
-        self.pub_target_type = self.create_publisher(Int8, 'target_t', 10)
+        self.pub_target_type = self.create_publisher(Int8, 'target_type', 10)
 
-        # Variables porque me pierdo xd
-        self.target_type = 0
+        self.get_target_type()
 
-        def ask_target_type(self, msg):
-            int valor = input ('Tipo de objetivo: ')
-            self.pub_target_type.publish(valor)
-        
-    def main(args=None):
-        rclpy.init(args=args)
-        target=Target()*
+    def get_target_type(self):
+        while rclpy.ok():
+            valor = int(input('Tipo de objetivo (-1 para salir): '))
+            if valor == -1:
+                break
+            msg = Int8()
+            msg.data = valor
+            self.pub_target_type.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    target = Target()
+    
+    try:
+        rclpy.spin(target)
+    except KeyboardInterrupt:
+        pass
+    
+    target.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
